9f94ce895bbb8354fcb462cc17b4f3dc6405ec6e
amirmatics <4miralbekord@gmail.com>
2025-09-17 18:01:17 -0700

    Setting up banner

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Card from "./components/Card"
 function App() {
   return (
     <>
+      <div className="banner">
+        <img src="https://static.vecteezy.com/system/resources/previews/002/876/939/non_2x/bubble-milk-tea-design-collection-pearl-milk-tea-boba-milk-tea-yummy-drinks-coffees-with-doodle-style-banner-illustration-vector.jpg" alt="boba banner" className="bannerImage" />
+        <h1 className="projectName">Boba Board</h1>
+      </div>
       <div className="cardsContainer">
         <Card
           img=""
